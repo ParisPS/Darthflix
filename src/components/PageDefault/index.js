12d08f5056d54699b3de69ed5f60115bb8fd19de
ParisPS <paris.perez.s@gmail.com>
2020-08-04 21:16:45 -0300

    commit

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -12,9 +12,6 @@ const Main = styled.main`
   padding-top: 50px;
   padding-left: 5%;
   padding-right: 5%;
-  ${({ paddingAll }) => css`
-    padding: ${paddingAll};
-    `}
 `;
 
 function PageDefault({ children, paddingAll }) {
